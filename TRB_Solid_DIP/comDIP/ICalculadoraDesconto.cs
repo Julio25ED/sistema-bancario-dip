d75acdcfa06109a7a751ea41b218112f4061a5d2
Julio Eduardo <141694709+Julio25ED@users.noreply.github.com>
2025-10-20 20:18:18 -0300

    Atualizar o ICalculadoraDesconto.cs

--- TRB_Solid_DIP/comDIP/ICalculadoraDesconto.cs
+++ TRB_Solid_DIP/comDIP/ICalculadoraDesconto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TRB_Solid_DIP.comDIP
 {
-    public interface ICalculadoraDesconto     //inversao de dependencia 
+    public interface ICalculadoraDesconto     //injeçao de dependencia 
     {                                           // depende de abstrações e não de classes concretas
         decimal CalcularDesconto(decimal valor, int anosCliente);
 
